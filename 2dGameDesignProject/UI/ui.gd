2b3c541ea0d858b6783a54ddd76d8a8e0e5524f0
maxxBearr <maxbernst3in@gmail.com>
2026-03-02 19:51:54 -0500

    Max's UI scene
    Added the UI scene with a health bar, and commented basics of how to tie it to the actual player health once the player is set up

---
+++ 2dGameDesignProject/UI/ui.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@onready var progress_bar: ProgressBar = %ProgressBar
+
+var currentPlayerHealth : float = 0.0 ##this should directly access the player health value, once the player exists
+var maxPlayerHealth : float = 100.0
+
+func _ready() -> void:
+	#currentPlayerHealth = PlayerClass.currentHealth
+	#maxPlayerHealth = PlayerClass.maxHealth
+	pass
+
+func _process(delta: float) -> void:
+	## there are cleaner ways to do this but am waiting to see how archtecture plays out since player has not been set up yet
+	progress_bar.value = currentPlayerHealth / maxPlayerHealth
